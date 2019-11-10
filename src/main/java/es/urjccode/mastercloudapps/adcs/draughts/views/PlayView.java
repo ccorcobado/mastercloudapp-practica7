6453550bdc2ca55d9b14e69d8fa1b459958502f4
Carlos <ccorcobado@gmail.com>
2019-11-10 20:45:38 +0100

    Cambio en playView

--- src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/PlayView.java
+++ src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/PlayView.java
@@ -16,9 +16,9 @@ public class PlayView extends SubView {
         GameView gameView = new GameView();
         do {
             String command = this.console.readString("Mueven las " + color + ": ");
-            int origin = Integer.parseInt(command.substring(0, 2));
-            int target = Integer.parseInt(command.substring(3, 5));
-            error = playController.move(new Coordinate(origin / 10 - 1, origin % 10 - 1), new Coordinate(target / 10 - 1, target % 10 - 1));
+            String origin = command.substring(0, 2);
+            String target = command.substring(3, 5);
+            error = playController.move(new Coordinate(origin), new Coordinate(target));
             if (error != null) {
                 this.console.writeln("Error!!!" + error.name());
                 gameView.write(playController);
